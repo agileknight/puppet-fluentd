76b637ff0649ceb4d0bbcb80054327823fb4e842
Adrian Huber <huber@smartcast.de>
2015-10-16 16:16:01 +0200

    use architecture amd64 for apt rep

--- manifests/install_repo/apt.pp
+++ manifests/install_repo/apt.pp
@@ -6,16 +6,17 @@
 class fluentd::install_repo::apt () {
 
     apt::source { 'treasure-data':
-        location    => "http://packages.treasuredata.com/2/ubuntu/trusty/",
-        release     => "trusty",
-        repos       => "contrib",
-        key 		=> {
+        location     => "http://packages.treasuredata.com/2/ubuntu/trusty/",
+        release      => "trusty",
+        repos        => "contrib",
+        architecture => 'amd64',
+        key 		 => {
         	'id'	  => '0xea124b4372c1cd00',
         	'source'  => 'https://packages.treasuredata.com/GPG-KEY-td-agent',
         	'include' => {
         		'src' => false,
         	},
         },
-        notify      => Exec['apt_update'],
+        notify       => Exec['apt_update'],
     }
 }
