a7fb48dbddc910fc8fa7cbe74227230116ba7914
Adrian Huber <huber@smartcast.de>
2015-11-03 14:38:56 +0100

    fixed quotes

--- manifests/install_plugin/wget.pp
+++ manifests/install_plugin/wget.pp
@@ -7,7 +7,7 @@ define fluentd::install_plugin::wget (
     fail('require url')
   }
 
-  exec { '$plugin_name':
+  exec { "$plugin_name":
     command => "/usr/bin/wget -O '/etc/td-agent/plugin/${plugin_name}' '${url}'",
     creates => "/etc/td-agent/plugin/${plugin_name}",
     require => Package[$fluentd::package_name],
