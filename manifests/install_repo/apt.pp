dd572a097ded151e78b94c9b8460b710040d7a62
Philipp Meier <mail@philippmeier.com>
2017-01-02 10:25:10 +0100

    Fix outdated gpg key

--- manifests/install_repo/apt.pp
+++ manifests/install_repo/apt.pp
@@ -6,17 +6,19 @@
 class fluentd::install_repo::apt () {
 
     apt::source { 'treasure-data':
-        location     => "http://packages.treasuredata.com/2/ubuntu/trusty/",
-        release      => "trusty",
-        repos        => "contrib",
-        architecture => 'amd64',
-        key 		 => {
-        	'id'	  => 'C901622B5EC4AF820C38AB861093DB45A12E206F',
-        	'source'  => 'https://packages.treasuredata.com/GPG-KEY-td-agent',
-        	'include' => {
-        		'src' => false,
-        	},
-        },
-        notify       => Exec['apt_update'],
+        location    => "http://packages.treasuredata.com/debian",
+        release     => "lucid",
+        repos       => "contrib",
+        include_src => false,
+    }
+
+    file { '/tmp/packages.treasure-data.com.key':
+        ensure => file,
+        source => 'puppet:///modules/fluentd/packages.treasure-data.com.key'
+    }->
+    exec { "import gpg key Treasure Data":
+        command => "/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -",
+        unless  => "/usr/bin/apt-key list | grep -q 'Treasure Data'",
+        notify  => Class['::apt::update'],
     }
 }
